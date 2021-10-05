b484280fe2443027037c6a415d407048619508cb
indenaiten <angel.herce.soto@gmail.com>
2021-10-05 10:59:28 +0200

    feature #04 - Spring Boot: Test de Servicios (Mockito) // #059 - Escribiendo tests assertThrow para afirmar que la excepción lanzada sea correcta. - The "setUp" method of "SpringbootTestApplicationTests" class has been updated - The "testTransferInterAccounts_NotEnoughMoneyException" test has been added in "SpringbootTestApplicationTests" class.

--- src/test/java/org/idnt/udemy/springboot/app/example/test/SpringbootTestApplicationTests.java
+++ src/test/java/org/idnt/udemy/springboot/app/example/test/SpringbootTestApplicationTests.java
@@ -1,12 +1,13 @@
 package org.idnt.udemy.springboot.app.example.test;
 
+import org.idnt.udemy.springboot.app.example.exception.NotEnoughMoneyException;
 import org.idnt.udemy.springboot.app.example.model.Account;
 import org.idnt.udemy.springboot.app.example.model.Bank;
 import org.idnt.udemy.springboot.app.example.repository.AccountRepository;
 import org.idnt.udemy.springboot.app.example.repository.BankRepository;
 import org.idnt.udemy.springboot.app.example.service.AccountService;
 import org.idnt.udemy.springboot.app.example.service.impl.AccountServiceImpl;
-import org.idnt.udemy.springboot.app.example.test.util.Data;
+import org.idnt.udemy.springboot.app.example.test.util.DATA;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -23,19 +25,11 @@ class SpringbootTestApplicationTests {
 	private BankRepository bankRepository;
 	private AccountService accountService;
 
-	private Account account001;
-	private Account account002;
-	private Bank bank;
-
 	@BeforeEach
 	void setUp() {
 		this.accountRepository = mock(AccountRepository.class);
 		this.bankRepository = mock(BankRepository.class);
 		this.accountService = new AccountServiceImpl(this.accountRepository, this.bankRepository);
-
-		this.account001 = new Account(Data.ACCOUNT_001.getId(), Data.ACCOUNT_001.getPersonName(), Data.ACCOUNT_001.getBalance());
-		this.account002 = new Account(Data.ACCOUNT_002.getId(), Data.ACCOUNT_002.getPersonName(), Data.ACCOUNT_002.getBalance());
-		this.bank = new Bank(Data.BANK.getId(), Data.BANK.getName(), Data.BANK.getTotalTransactions());
 	}
 
 	@Test
@@ -46,9 +40,9 @@ class SpringbootTestApplicationTests {
 		final Long ID_ACCOUNT_TARGET = 2L;
 		final Long ID_BANK = 1L;
 
-		when(this.accountService.findById(ID_ACCOUNT_ORIGIN)).thenReturn(this.account001);
-		when(this.accountService.findById(ID_ACCOUNT_TARGET)).thenReturn(this.account002);
-		when(this.bankRepository.findById(ID_BANK)).thenReturn(this.bank);
+		when(this.accountService.findById(ID_ACCOUNT_ORIGIN)).thenReturn(DATA.getAccount001());
+		when(this.accountService.findById(ID_ACCOUNT_TARGET)).thenReturn(DATA.getAccount002());
+		when(this.bankRepository.findById(ID_BANK)).thenReturn(DATA.getBank());
 
 		//When - Get balances from accounts and get total transactions from bank
 		BigDecimal balanceOrigin = this.accountService.checkBalance(ID_ACCOUNT_ORIGIN);
@@ -79,4 +73,48 @@ class SpringbootTestApplicationTests {
 		verify(this.bankRepository).update(any(Bank.class));
 	}
 
+	@Test
+	@DisplayName("Test that checks that the \"NotEnoughMoneyException\" exception is thrown when a transfer is made between accounts when the originating account does not have enough money.")
+	void testTransferInterAccounts_NotEnoughMoneyException() {
+		//Given
+		final Long ID_ACCOUNT_ORIGIN = 1L;
+		final Long ID_ACCOUNT_TARGET = 2L;
+		final Long ID_BANK = 1L;
+
+		when(this.accountService.findById(ID_ACCOUNT_ORIGIN)).thenReturn(DATA.getAccount001());
+		when(this.accountService.findById(ID_ACCOUNT_TARGET)).thenReturn(DATA.getAccount002());
+		when(this.bankRepository.findById(ID_BANK)).thenReturn(DATA.getBank());
+
+		//When - Get balances from accounts and get total transactions from bank
+		BigDecimal balanceOrigin = this.accountService.checkBalance(ID_ACCOUNT_ORIGIN);
+		BigDecimal balanceTarget = this.accountService.checkBalance(ID_ACCOUNT_TARGET);
+		int totalTransactions = this.accountService.checkTotalTransactions(ID_BANK);
+
+		//Then - Checks balances and total transactions
+		assertEquals("1000", balanceOrigin.toPlainString());
+		assertEquals("2000", balanceTarget.toPlainString());
+		assertEquals(0, totalTransactions);
+
+		//When - Transfer more than the balance in the source account.
+		assertThrows(NotEnoughMoneyException.class, () -> {
+			this.accountService.transfer(ID_BANK, ID_ACCOUNT_ORIGIN, ID_ACCOUNT_TARGET, new BigDecimal("1500"));
+		});
+
+		//When - Get new balances from accounts and get total transactions from bank.
+		BigDecimal newBalanceOrigin = this.accountService.checkBalance(ID_ACCOUNT_ORIGIN);
+		BigDecimal newBalanceTarget = this.accountService.checkBalance(ID_ACCOUNT_TARGET);
+		totalTransactions = this.accountService.checkTotalTransactions(ID_BANK);
+
+		//Then - Checks new balances and total transactions
+		assertEquals("1000", newBalanceOrigin.toPlainString());
+		assertEquals("2000", newBalanceTarget.toPlainString());
+		assertEquals(0, totalTransactions);
+
+		//Then - Verifiy calls methods
+		verify(this.accountRepository, times(3)).findByid(ID_ACCOUNT_ORIGIN);
+		verify(this.accountRepository, times(2)).findByid(ID_ACCOUNT_TARGET);
+		verify(this.accountRepository, times(0)).update(any(Account.class));
+		verify(this.bankRepository, times(2)).findById(ID_BANK);
+		verify(this.bankRepository, times(0)).update(any(Bank.class));
+	}
 }
