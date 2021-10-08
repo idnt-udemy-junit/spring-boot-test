a88d2e3c493abdd31a17811280fbbd145d359fa1
indenaiten <angel.herce.soto@gmail.com>
2021-10-08 10:37:30 +0200

    feature #05 - Spring Boot: Test de Repositorios (DataJpaTest) // #065 - Configurando el contexto de persistencia JPA y clases entities para test The "import.sql" file has been added to aggregate test data to h2 database.

---
+++ src/test/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO ACCOUNTS (personName, balance)
+    VALUES ("Andrés", 1000),
+    VALUES ("Jhon", 2000);
+
+INSERT INTO BANKS (name, totalTransactions)
+    VALUES ("El banco financiero", 0);
