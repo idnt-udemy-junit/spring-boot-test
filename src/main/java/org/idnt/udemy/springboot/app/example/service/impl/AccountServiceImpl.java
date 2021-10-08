201df2c162e88f8dc5b143769fc86b6704edbe6d
indenaiten <angel.herce.soto@gmail.com>
2021-10-08 11:59:16 +0200

    feature #05 - Spring Boot: Test de Repositorios (DataJpaTest) // #067 - Modificando nuestros repositorios con Spring Data JPA The "AccountServiceImpl" class has been updated to solve some errors caused that update of repositories.

--- src/main/java/org/idnt/udemy/springboot/app/example/service/impl/AccountServiceImpl.java
+++ src/main/java/org/idnt/udemy/springboot/app/example/service/impl/AccountServiceImpl.java
@@ -20,34 +20,34 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Account findById(final Long idAccount) {
-        return this.accountRepository.findById(idAccount);
+        return this.accountRepository.findById(idAccount).orElseThrow();
     }
 
     @Override
     public int checkTotalTransactions(final Long idBank) {
-        Bank bank = this.bankRepository.findById(idBank);
+        Bank bank = this.bankRepository.findById(idBank).orElseThrow();
         return bank.getTotalTransactions();
     }
 
     @Override
     public BigDecimal checkBalance(final Long idAccount) {
-        Account account = this.accountRepository.findById(idAccount);
+        Account account = this.accountRepository.findById(idAccount).orElseThrow();
         return account.getBalance();
     }
 
     @Override
     public void transfer(final Long idBank, final Long idAccountOrigin, final Long idAccountTarget, final BigDecimal quantity) {
-        Account accountOrigin = this.accountRepository.findById(idAccountOrigin);
+        Account accountOrigin = this.accountRepository.findById(idAccountOrigin).orElseThrow();
         accountOrigin.debit(quantity);
-        this.accountRepository.update(accountOrigin);
+        this.accountRepository.save(accountOrigin);
 
-        Account accountTarget = this.accountRepository.findById(idAccountTarget);
+        Account accountTarget = this.accountRepository.findById(idAccountTarget).orElseThrow();
         accountTarget.credit(quantity);
-        this.accountRepository.update(accountTarget);
+        this.accountRepository.save(accountTarget);
 
-        Bank bank = this.bankRepository.findById(idBank);
+        Bank bank = this.bankRepository.findById(idBank).orElseThrow();
         int totalTransactions = bank.getTotalTransactions();
         bank.setTotalTransactions(++totalTransactions);
-        this.bankRepository.update(bank);
+        this.bankRepository.save(bank);
     }
 }
