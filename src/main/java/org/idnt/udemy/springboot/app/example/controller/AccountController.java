08e69d6a2e955dda4d6a7813b99f151db7c0af4e
indenaiten <angel.herce.soto@gmail.com>
2021-10-13 21:38:03 +0200

    feature #06 - Spring Boot: Test de Controladores con MockMvc (WebMvcTest) // #075 - Configurando Swagger The controller has been updated to "RestController".

--- src/main/java/org/idnt/udemy/springboot/app/example/controller/AccountController.java
+++ src/main/java/org/idnt/udemy/springboot/app/example/controller/AccountController.java
@@ -6,15 +6,13 @@ import org.idnt.udemy.springboot.app.example.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
     @Autowired
